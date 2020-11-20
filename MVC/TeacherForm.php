fb63cbce0ce374a2d26795210b7222fa0d1f83e2
DESKTOP-8RAM7EQ\chen <s106213077@mail1.ncnu.edu.tw>
2020-11-20 20:53:38 +0800

    teacher complete

--- MVC/TeacherForm.php
+++ MVC/TeacherForm.php
@@ -15,7 +15,7 @@ if (! $rs) {
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>無標題文件</title>
+<title>TeacherForm</title>
 </head>
 <body>
 <h1>Edit Task</h1>
