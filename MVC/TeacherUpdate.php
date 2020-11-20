30be27fd193072ef4037de810b8a85c1b5996f98
DESKTOP-8RAM7EQ\chen <s106213077@mail1.ncnu.edu.tw>
2020-11-20 20:39:39 +0800

    teacher complete

--- MVC/TeacherUpdate.php
+++ MVC/TeacherUpdate.php
@@ -1,10 +1,8 @@
 <?php
 require("todoModel.php");
-
-$id = mysqli_real_escape_string($conn,$_POST['id']);
+$stuID = mysqli_real_escape_string($conn,$_POST['stuID']);
 $teacher_Comment = mysqli_real_escape_string($conn,$_POST['teacher_Comment']);
 $teacher_Agree = mysqli_real_escape_string($conn,$_POST['teacher_Agree']);
-//echo $id,$teacher_Comment,$teacher_Agree;
-updateList($id,$teacher_Comment,$teacher_Agree);
+teacherUpdateList($stuID,$teacher_Comment,$teacher_Agree);
 header("Location: TeacherMain.php");
 ?>
