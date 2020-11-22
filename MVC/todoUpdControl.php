8b983dc39088863e05fb4fb5a345be1abcf7085c
MSI\user <s107213015@mail1.ncnu.edu.tw>
2020-11-22 22:39:16 +0800

    1122

--- MVC/todoUpdControl.php
+++ MVC/todoUpdControl.php
@@ -4,15 +4,14 @@ require("todoModel.php");
 $name=mysqli_real_escape_string($conn,$_POST['name']);
 $dad_name=mysqli_real_escape_string($conn,$_POST['dad_name']);
 $mom_name=mysqli_real_escape_string($conn,$_POST['mom_name']);
-$subsidyType=mysqli_real_escape_string($conn,$_POST['subsidyType']);
+$subsidyType=mysqli_real_escape_string($conn,$_POST['subsidy_type']);
 $stuID=(int)$_POST['stuID'];
 
 
-if ($title) { //if title is not empty
+if ($name != NULL) { //if title is not empty
 	updateJob($stuID,$name, $dad_name, $mom_name, $subsidyType);
 	$msg="Message updateded";
 } else {
 	$msg= "Message title cannot be empty";
 }
 header("Location: todoListView.php?m=$msg");
-?>
